d0d7fc03872e08c34cc0a3017b55d4cf92448dae
erviadkha <erviadkha@gmail.com>
2021-10-22 08:31:59 +0700

    First Commmit

---
+++ TokoBangunan/Controllers/TokoBangunan.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using TokoBangunan.Models; 
+using System.Text.Encodings.Web;
+
+namespace TokoBangunan.Controllers
+{
+    public class TokoBangunan : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+    }
+}
