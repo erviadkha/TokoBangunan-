6b944262cf62e9791f4e19d0fe46a6ca62d6dadc
erviadkha <erviadkha@gmail.com>
2021-11-08 10:29:00 +0700

    tambahan pada fitur

--- TokoBangunan/Controllers/TokoBangunan.cs
+++ TokoBangunan/Controllers/TokoBangunan.cs
@@ -7,17 +7,29 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using TokoBangunan.Models; 
 using System.Text.Encodings.Web;
+using TokoBangunan.Data;
 
 namespace TokoBangunan.Controllers
 {
     public class TokoBangunan : Controller
     {
+        public TokoBangunanDbContext _context;
+        public TokoBangunan(TokoBangunanDbContext context)
+        {
+            _context = context;
+        }
 
         public IActionResult Index()
         {
-            return View();
+            var list = _context.Produks.ToList();
+            return View(list);
         }
 
+        public IActionResult Details(int id)
+        {
+            var Produk = _context.Produks.Find(id);
+            return View(Produk);
+        }
         
 
         public IActionResult Privacy()
